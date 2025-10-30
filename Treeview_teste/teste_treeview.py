f7d3a026def3bf0f50227764c164776917d28d2b
Julia Cereda <julia.cereda@aluno.senai.br>
2025-10-30 15:55:17 -0300

    comity: treeview

--- Treeview_teste/teste_treeview.py
+++ Treeview_teste/teste_treeview.py
@@ -1,5 +1,9 @@
 import ttkbootstrap as ttk
 
+def apagar_item():
+    item_selecionado = treeview.selection()
+    treeview.delete(item_selecionado)
+
 janela = ttk.Window(themename="vapor")
 
 treeview = ttk.Treeview(janela)
@@ -8,11 +12,18 @@ treeview.pack()
 #se voce precisae que a coluna seja maior, vai mudar atraves do nome dado a coluna
 treeview["columns"] = ("nome", "idade", "cidade")
 
+treeview["show"] = "headings"
 treeview.heading("nome", text="Nome completo")
-treeview.heading("", text="Nome completo")
+treeview.heading("idade", text="Idade")
+treeview.heading("cidade", text="Cidade")
+
+treeview.insert("","end", values=["godofredo", "3", "matao"])
+
+ttk.Button(janela, text= "DELETAR", command=apagar_item).pack()
 
 
 
 
-
+treeview.column("idade",width=200, anchor="center")
+treeview.insert("","end", values=["Godofredo", "3", "matao"])
 janela.mainloop()
